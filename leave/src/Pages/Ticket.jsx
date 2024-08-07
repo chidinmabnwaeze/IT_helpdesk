e7c48b06f4dfc7e4199dd4ad7f7d1faba304499d
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-07 11:56:15 +0100

    ticket

--- leave/src/Pages/Ticket.jsx
+++ leave/src/Pages/Ticket.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 export default function Ticket() {
+    
   return (
-    <div className="ticket bg-white">
+    <section className="ticket bg-white">
       <div className="top-sect">
+        <div>
         <h2>My Tickets</h2>
+        </div>
+        <section>
+        <div className="tabs">
+            <ul className="tab">
+                <li>All Issues</li>
+                <li>New Issues</li>
+                <li>Opened Issues</li>
+                <li>Closed Issues</li>
+            </ul>
+        </div>
+        <div className="date"></div>
+        </section>
       </div>
-    </div>
+    </section>
   );
 }
