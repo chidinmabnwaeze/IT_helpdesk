8b291520095a589c9b7fff36947b65b064346bc6
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-07 13:53:10 +0100

    top-section

--- leave/src/Pages/Ticket.jsx
+++ leave/src/Pages/Ticket.jsx
@@ -1,25 +1,32 @@
 import React from "react";
+import Topbar from "../Components/Topbar";
+import Sidebar from "../Components/Sidebar";
 
-export default function Ticket(){
-
+export default function Ticket() {
   return (
-    <div className="ticket bg-white">
-      <div className="top-sect">
-        <div>
-        <h2>My Tickets</h2>
+    <div className="ticket">
+      <Topbar />
+      <Sidebar />
+      <section className="ticket-body m-25 ">
+        <div className="top-sect">
+          <div>
+            <h2>My Tickets</h2>
+          </div>
+          <section>
+            <div className="tabs flex mt-3 ">
+              <ul className="tab flex">
+                <li className="tab-list">All Issues</li>
+                <li className="tab-list">New Issues</li>
+                <li className="tab-list">Opened Issues</li>
+                <li className="tab-list">Closed Issues</li>
+              </ul>
+            </div>
+            <div className="date"></div>
+          </section>
+          <div className="ticket-count font-medium">365 Tickets</div>
+          <hr />
         </div>
-        <section>
-        <div className="tabs">
-            <ul className="tab">
-                <li>All Issues</li>
-                <li>New Issues</li>
-                <li>Opened Issues</li>
-                <li>Closed Issues</li>
-            </ul>
-        </div>
-        <div className="date"></div>
-        </section>
-      </div>
+      </section>
     </div>
   );
 }
