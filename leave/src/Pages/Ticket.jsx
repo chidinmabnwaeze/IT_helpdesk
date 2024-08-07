24e8461afba27ee385076f060826a0351c5f1c67
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-07 11:45:16 +0100

    ticket

---
+++ leave/src/Pages/Ticket.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Ticket() {
+  return (
+    <div className="ticket bg-white">
+      <div className="top-sect">
+        <h2>My Tickets</h2>
+      </div>
+    </div>
+  );
+}
