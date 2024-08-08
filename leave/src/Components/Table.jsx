15796fbca9d0d277d9f75950c61902a9a6993dd3
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-08 02:01:58 +0100

    update

--- leave/src/Components/Table.jsx
+++ leave/src/Components/Table.jsx
@@ -1,33 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Table(){
-    return(
-        <table className="Table">
-<tr>
-    <th>Complaint</th>
-    <th>Issues</th>
-    <th>Priority</th>
-</tr>
-<tr>
-    <td>
-        <div className="checkbox">
+export default function Table() {
+    const [button, setButton] = useState([
+    {
+        button: "Accept"
+    }
+    ])
+  return (
+    <table className="Table">
+      <tr>
+        <th>Complaint</th>
+        <th>Issues</th>
+        <th>Priority</th>
+      </tr>
+
+      {}
+      <tr>
+        <td>
+          <div className="checkbox">
             <input type="checkbox" id="complaint" />
-        </div>
-        <div className="name">
+          </div>
+          <div className="name">
             <p>Mary John</p>
             <p className="bg-grey-200">maryjohn@gmail.com</p>
-        </div>
-    </td>
-    <td>
-        <p>Mylaptop is not functioning...</p>
-    </td>
-    <td>
-        <div className="priority">
+          </div>
+        </td>
+        <td>
+          <p>Mylaptop is not functioning...</p>
+        </td>
+        <td>
+          <div className="priority">
             <div className="rounded bg-red-500"></div>
             <p className="bg-red-500">High</p>
-        </div>
-    </td>
-</tr>
-        </table>
-    )
+          </div>
+        </td>
+        <button>Accept</button>
+      </tr>
+    </table>
+  );
 }
