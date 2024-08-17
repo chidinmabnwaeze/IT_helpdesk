9c034f09ecaf8d5f5146b39c5c324dc4cd31805a
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-17 07:39:57 +0100

    complaint

---
+++ leave/src/Pages/Complaint.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Topbar from "../Components/Topbar";
+import Sidebar from "../Components/Sidebar";
+
+const Complaint = () => {
+  return (
+    <div>
+      <Topbar />
+      <Sidebar />
+
+      <div>
+        <div>
+          <p>Mary John</p>
+          <p>maryjohn@gmail.com</p>
+        </div>
+        <div>
+            <p>{tabb.status}</p></div>
+      </div>
+    </div>
+  );
+};
+
+export default Complaint;
