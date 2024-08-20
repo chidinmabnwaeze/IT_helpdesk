0045f944407f41023d0e5ac3c510acc258967bda
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-20 16:47:08 +0100

    notify

--- leave/src/Components/Notification.jsx
+++ leave/src/Components/Notification.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Notification = () => {
   return (
     <div>
+      <div>
+        <p>New notification</p>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <p>You have been assigned to a live session with a waiting client</p>
         <div>
-            <p>New notification</p>
-            <img src="" alt="" />
+          <button className="accept py-1.5 px-5  rounded-md">Accept</button>
+          <button className="assign py-1.5 px-5  rounded-md">Assign</button>
         </div>
-        <div>
-            <p>You have been assigned to a live session with a waiting client</p>
-            <div>
-            <button className="accept py-1.5 px-5  rounded-md">Accept</button>
-    <button className="assign py-1.5 px-5  rounded-md">Assign</button>
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
