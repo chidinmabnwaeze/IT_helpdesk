07021fa78abf1813287a2b2dc171c8248bf66c86
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-20 16:46:38 +0100

    button

--- leave/src/Components/Notification.jsx
+++ leave/src/Components/Notification.jsx
@@ -7,6 +7,13 @@ const Notification = () => {
             <p>New notification</p>
             <img src="" alt="" />
         </div>
+        <div>
+            <p>You have been assigned to a live session with a waiting client</p>
+            <div>
+            <button className="accept py-1.5 px-5  rounded-md">Accept</button>
+    <button className="assign py-1.5 px-5  rounded-md">Assign</button>
+            </div>
+        </div>
     </div>
   )
 }
