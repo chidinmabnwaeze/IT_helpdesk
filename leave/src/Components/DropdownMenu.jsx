68fe12917ad0a48fac46897076c59b820a7149e7
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-08-28 17:12:46 +0100

    menu

--- leave/src/Components/DropdownMenu.jsx
+++ leave/src/Components/DropdownMenu.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import DropdownButton from "./DropdownButton";
 import DropdownItems from "./DropdownItems";
 
-const DropdownMenu = ({buttonText, content}) => {
-const [open, setOpen] = useState(false);
-const toggleDown =() =>{
-    setOpen((open)=>!open)
-}
+const DropdownMenu = ({ buttonText, content }) => {
+  const [open, setOpen] = useState(false);
+  const toggleDown = () => {
+    setOpen((open) => !open);
+  };
+  const dropdownRef = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+      document.addEventListener("click" ,handler)
+
+      return () =>{
+      document.removeEventListener("click", handler)
+    }
+  }, [dropdownRef]);
 
   return (
-    <div className="relative">
-      <DropdownButton open ={open} 
-      toggle={toggleDown}>
+    <div className="relative" ref={dropdownRef}>
+      <DropdownButton open={open} toggle={toggleDown}>
         {buttonText}
-        </DropdownButton>
-      <DropdownItems open ={open}>
-        {content}
-        </DropdownItems>
+      </DropdownButton>
+      <DropdownItems open={open}>{content}</DropdownItems>
     </div>
   );
 };
