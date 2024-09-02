7283ddc1949cd2c1b6fc28f09456f91b07ec425c
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-09-02 22:33:09 +0100

    form

--- userTicket/src/App.jsx
+++ userTicket/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Topbar from '../../leave/src/Components/Topbar'
+import CompliantForm from './Pages.jsx/CompliantForm'
 
 function App() {
 
 
   return (
     <>
-      
+     <BrowserRouter>
+     <Routes>
+      <Route index ='/form' element ={<CompliantForm/>}></Route>
+      <Route path ='/form' element ={<CompliantForm/>}></Route>
+     </Routes>
+     </BrowserRouter> 
     </>
   )
 }
---
+++ userTicket/src/Pages.jsx/CompliantForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CompliantForm = () => {
+  return (
+    <div>
+        <form action="">
+            <div>
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" />
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default CompliantForm
